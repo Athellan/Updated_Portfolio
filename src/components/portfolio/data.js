abacbcc0ca8b0d98d615e783f97077b1c23d2366
Jean-Baptiste CAMBILLARD <jeanbaptiste.cambillard@gmail.com>
2022-07-22 00:18:38 +0200

    gif tested

--- src/components/portfolio/data.js
+++ src/components/portfolio/data.js
@@ -1,5 +1,7 @@
-import IMG1 from "../../assets/portfolio1.jpg";
-import IMG2 from "../../assets/portfolio2.jpg";
+// import IMG1 from "../../assets/portfolio1.jpg";
+import IMG1 from "../../assets/wip1.gif";
+// import IMG2 from "../../assets/portfolio2.jpg";
+import IMG2 from "../../assets/wip3.gif";
 import IMG3 from "../../assets/portfolio3.jpg";
 import IMG4 from "../../assets/portfolio4.jpg";
 import IMG5 from "../../assets/portfolio5.png";
@@ -11,48 +13,42 @@ const data = [
     image: IMG1,
     title: "Titre 1",
     github: "https://github.com",
-    demo:
-      "https://dribbble.com/shots/18672349-Orion-UI-kit-Charts-templates-infographics-in-Figma",
+    demo: "https://tenor.com/view/rylsee-tooshytorap-work-in-progress-work-workout-gif-22724772",
   },
   {
     id: 2,
     image: IMG2,
     title: "Titre 2",
     github: "https://github.com",
-    demo:
-      "https://dribbble.com/shots/18672453-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps",
+    demo: "https://dribbble.com/shots/18672453-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps",
   },
   {
     id: 3,
     image: IMG3,
     title: "Titre 3",
     github: "https://github.com",
-    demo:
-      "https://dribbble.com/shots/18672344-Orion-UI-kit-Charts-templates-infographics-in-Figma",
+    demo: "https://dribbble.com/shots/18672344-Orion-UI-kit-Charts-templates-infographics-in-Figma",
   },
   {
     id: 4,
     image: IMG4,
     title: "Titre 4",
     github: "https://github.com",
-    demo:
-      "https://dribbble.com/shots/18672443-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps",
+    demo: "https://dribbble.com/shots/18672443-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps",
   },
   {
     id: 5,
     image: IMG5,
     title: "Titre 5",
     github: "https://github.com",
-    demo:
-      "https://dribbble.com/shots/18672227-Orion-UI-kit-Charts-templates-infographics-in-Figma",
+    demo: "https://dribbble.com/shots/18672227-Orion-UI-kit-Charts-templates-infographics-in-Figma",
   },
   {
     id: 6,
     image: IMG6,
     title: "Titre 6",
     github: "https://github.com",
-    demo:
-      "https://dribbble.com/shots/18672157-Orion-UI-kit-Charts-templates-infographics-in-Figma",
+    demo: "https://dribbble.com/shots/18672157-Orion-UI-kit-Charts-templates-infographics-in-Figma",
   },
 ];
 
